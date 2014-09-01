6cd129a0323e33a1f2960fdf6976c07e9378c2d9
tterrag1098 <tterrag1098@gmail.com>
2014-09-01 04:11:35 -0400

    Correct version number

--- src/main/java/org/wyldmods/kitchencraft/common/lib/Reference.java
+++ src/main/java/org/wyldmods/kitchencraft/common/lib/Reference.java
@@ -15,5 +15,5 @@ public class Reference
     public static final String MOD_TEXTUREPATH = "kitchencraft";
     public static final String VERSION = "0.0.1";
     
-    public static final String DEPENDENCIES = "required-after:ttCore@MC1.7.10-0.0.2-5,)";
+    public static final String DEPENDENCIES = "required-after:ttCore@1.7.10-0.0.2-5,)";
 }
